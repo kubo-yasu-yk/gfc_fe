cac4b1702c57e5186405946b65ef26f56d723e73
Yasuhiro Kubota <yasuhiro.kubota0716ykyk@gmail.com>
2022-10-01 19:11:56 +0900

    login.tsxの静的実装完了

--- components/registration/InputPassword.tsx
+++ components/registration/InputPassword.tsx
@@ -3,11 +3,12 @@ interface Props {
   width: string;
 }
 
-export function Input(props: Props) {
+export function InputPassword(props: Props) {
   return (
     <input
       class={`text-[11px] ml-6 mt-4 mb-4 pl-2 w-[${props.width}] h-10 rounded-[5px] border-[#C3C3C3] border-[1px]`}
       placeholder={props.placeholder}
+      type="password"
     />
   );
 }
--- routes/login.tsx
+++ routes/login.tsx
@@ -2,21 +2,33 @@ import { MainHeader } from "../components/common/MainHeader.tsx";
 import { Title } from "../components/registration/Title.tsx";
 import { SubFooter } from "../components/common/SubFooter.tsx";
 import { BrownButton } from "../components/common/BrownButton.tsx";
+import { Input } from "../components/registration/Input.tsx";
+import { InputPassword } from "../components/registration/InputPassword.tsx";
 
 export default function Login() {
   return (
     <>
       <MainHeader />
+
       <Title title="ログイン" />
 
-      メールアドレス パスワード
+      <p class="text-[12px] text-[#5F5F5F] text-left font-bold ml-4 mt-6">
+        メールアドレス
+      </p>
+      <Input placeholder="insyoku.franchise@insyoku.co.jp" width="20rem" />
 
-      <p class="text-[10px] text-[#947771] text-right mr-4 font-bold">
+      <p class="text-[12px] text-[#5F5F5F] text-left font-bold ml-4 mt-3">
+        パスワード
+      </p>
+      <InputPassword placeholder="半角英数字の6~20文字" width="20rem" />
+
+      <p class="text-[10px] text-[#947771] text-right mr-4 mb-8 font-bold">
         パスワードを表示する
       </p>
 
       <BrownButton contents="ログイン" mandatory={false} />
-      <p class="text-[10px] text-[#947771] text-center font-bold">
+
+      <p class="text-[10px] text-[#947771] text-center font-bold mb-6">
         初めてのご利用の方（新規会員登録）
       </p>
 
@@ -41,6 +53,7 @@ export default function Login() {
           パスワードをお忘れですか？
         </p>
       </div>
+
       <SubFooter />
     </>
   );
