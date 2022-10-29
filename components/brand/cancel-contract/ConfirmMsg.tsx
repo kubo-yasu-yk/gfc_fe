ec0fb5d835ab8e784452cc949361482394fae620
Daichi Natsume <taylor.1d.soccer@icloud.com>
2022-10-30 01:18:54 +0900

    ブランド申請解約キャンセルなど一通り

---
+++ components/brand/cancel-contract/ConfirmMsg.tsx
@@ -0,0 +1,8 @@
+export function ConfirmMsg() {
+  return (
+    <div class="text-[0.625rem] text-[#5F5F5F] my-[2rem] ml-[1rem] font-semibold">
+      <p>以下の内容で解約申請します。</p>
+      <p>よろしければ「解約申請する」ボタンを押してください。</p>
+    </div>
+  );
+}
---
+++ routes/brand/contract/completion.tsx
@@ -0,0 +1,21 @@
+import { MainHeader } from "../../../components/common/MainHeader.tsx";
+import { CompBanner } from "../../../components/brand/contract/CompBanner.tsx";
+import { Title } from "../../../components/brand/common/Title.tsx";
+import {
+  BrownButton2,
+  WhiteButton,
+} from "../../../components/brand/common/Button.tsx";
+import { SubFooter } from "../../../components/common/SubFooter.tsx";
+
+export default function Completion() {
+  return (
+    <>
+      <MainHeader />
+      <Title title="契約完了" />
+      <CompBanner />
+      <BrownButton2 link="/" contents="トップページへ" />
+      <WhiteButton link="/mypage" contents="マイページへ" />
+      <SubFooter />
+    </>
+  );
+}
