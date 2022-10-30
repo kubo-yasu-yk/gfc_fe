6e552ee393780f07dcad9786034fb12938faccad
Daichi Natsume <taylor.1d.soccer@icloud.com>
2022-10-30 18:21:01 +0900

    brand

--- islands/BrandPhoto.tsx
+++ islands/BrandPhoto.tsx
@@ -1,16 +1,15 @@
-// deno-lint-ignore-file
 import { Component } from "preact";
 
-// deno-lint-ignore no-explicit-any
+// deno-lint-ignore no-var no-explicit-any
 declare var Swiper: any;
 
 const imgs = [
-  "/icon/brand/brandPhoto0",
-  "/icon/brand/brandPhoto1",
-  "/icon/brand/brandPhoto2",
-  "/icon/brand/brandPhoto3",
-  "/icon/brand/brandPhoto4",
-  "/icon/brand/brandPhoto5",
+  "/icon/brand/brandPhoto0.png",
+  "/icon/brand/brandPhoto1.jpeg",
+  "/icon/brand/brandPhoto2.jpeg",
+  "/icon/brand/brandPhoto3.jpeg",
+  "/icon/brand/brandPhoto4.jpeg",
+  "/icon/brand/brandPhoto5.jpeg",
 ];
 
 export default class BrandPhoto extends Component {
@@ -19,26 +18,35 @@ export default class BrandPhoto extends Component {
       loop: true,
       speed: 500,
       centeredSlides: true,
-      spaceBetween: 1,
-      slidesPerView: 1.3,
-      autoplay: { //自動再生する
-        delay: 5000, //次のスライドに切り替わるまでの時間
-        disableOnInteraction: false, //ユーザーが操作したら止めるか
-        waitForTransition: false, // アニメーションの間にスライドを止めるか
+      autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+        waitForTransition: false,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
       },
     });
   }
   render() {
     return (
       <>
-        <div class="relative w-full h-[19rem] mt-8 border border-[#947771]">
-          <div class="swiper-container overflow-hidden">
-            <div class="swiper-wrapper">
-              {imgs.map((img) => <img src={img} alt="ブランドの写真" />)}
+        <div class="relative h-[19rem] mt-8 border border-[#947771] flex items-center">
+          <div class="h-[17rem] swiper-container overflow-hidden">
+            <div class="swiper-wrapper h-full">
+              {imgs.map((img) => (
+                <img
+                  src={img}
+                  alt="ブランドの写真"
+                  class="object-contain swiper-slide"
+                />
+              ))}
             </div>
-            <div class="swiper-pagination" />
+            <img src="/icon/brand/leftArrow.png" class="swiper-button-prev" />
+            <img src="/icon/brand/rightArrow.png" class="swiper-button-next" />
           </div>
-          <div class="w-14 h-14 border border-[#947771] rounded-full flex items-center absolute bottom-2 right-2">
+          <div class="w-14 h-14 border border-[#947771] rounded-full flex items-center absolute bottom-2 right-2 z-50 bg-white">
             <div class="flex flex-col text-center mx-auto">
               <img
                 src="/icon/brand/favorite.png"
